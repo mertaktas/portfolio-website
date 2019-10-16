dc0155936c22cf9a3dd7ed845d04d3234ce395ba
mert <mertaktas231@gmail.com>
2019-10-16 14:23:46 +0300

    typed.js eklendi ve düzenlendi.

---
+++ dist/js/typewrite.js
@@ -0,0 +1,7 @@
+var typed = new Typed('.type', {
+    strings: ['Designer.', 'Developer.'],
+    loop: true, //Döngü
+    typeSpeed: 80, //Hızı
+    backSpeed: 80, // Geridönüş Hızı
+    fadeOut: false //Solma işlemi
+});
